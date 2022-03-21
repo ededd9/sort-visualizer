ed0c45be0e8d88013e4032079e94f84b6a146d5e
Edward Hurtado <edhur9@gmail.com>
2022-03-20 23:52:39 -0400

    finished merge algorithm, with a helper function called 'merge'

--- src/algos.js
+++ src/algos.js
@@ -1,11 +1,35 @@
 import React from "react";
+function merge(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  let sortedArr = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      sortedArr.push(arr1[i]);
+      i++;
+    } else {
+      sortedArr.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    sortedArr.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    sortedArr.push(arr2[j]);
+    j++;
+  }
+  return sortedArr;
+  console.log(sortedArr);
+}
 
-const MergeSort = (arr) => {
-  //getting middle index to split original array into halves
+function MergeSort(arr) {
+  if (arr.length <= 1) return arr;
   const mid = Math.floor(arr.length / 2);
-  const firstPart = arr.slice(0, mid);
-  const secondPart = arr.slice(mid);
-  console.log(firstPart, secondPart);
-};
+  const firstPart = MergeSort(arr.slice(0, mid));
+  const secondPart = MergeSort(arr.slice(mid));
+  return merge(firstPart, secondPart);
+}
 
 export default MergeSort;
