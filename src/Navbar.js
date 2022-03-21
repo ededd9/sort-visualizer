bc9033ef4ca3302be99a489840365ee847ca9e06
Edward Hurtado <edhur9@gmail.com>
2022-03-20 23:54:28 -0400

    passed down mergedArr props and called it onClick

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Visualizer from "./Visualizer";
+import MergeSort from "./algos";
 const Navbar = (props) => {
-  const { arr, newArray } = props;
+  const { arr, newArray, mergedArr } = props;
 
   return (
     <>
       <div className="navbar">
         <header>Sort Visualizer</header>
         <button onClick={newArray}>New Array</button>
-        <button>Merge Sort</button>
+        <button onClick={mergedArr}>Merge Sort</button>
       </div>
     </>
   );
